aa1beb2cd4dc16352708879259b9f8cc0264f7d4
Yusuf Bilge SALDAMLI <yubilsal@gmail.com>
2024-05-07 20:41:51 +0300

    ttesttttt

--- nested-app/vite.config.js
+++ nested-app/vite.config.js
@@ -4,5 +4,8 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: "/nested-app/"
+  server: {
+    host: true,
+    port: 8080
+  }
 })
